94434e6ca1baa586379bead75cbc27151be22909
NadiaBCarmon <nadia.prysmmagazine@gmail.com>
2018-01-15 00:55:16 -0500

    Added Blank Slate Desktop app

--- src/App.js
+++ src/App.js
@@ -1,66 +1,59 @@
 import React, { Component } from 'react';
-import home from './home.jpg';
+//import home from './home.jpg';
 import './App.css';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 //import { Text, View, Image, StyleSheet, } from 'react-native';
 //import { Constants } from 'expo';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+//import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import brain from './brain.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      	<div className="Nav">
+      	<a href="App.js"><img className="Brain" src={brain} /></a>
 
-    <div className="Navbar">
-	<Navbar inverse collapseOnSelect>
-		<Navbar.Header className="Header">
-			<Navbar.Brand className="BackHome" >
-				<a href="App.js"><img className="Brain" src={brain}/></a>
-			</Navbar.Brand>
-		</Navbar.Header>
-		<Navbar.Collapse>
 
-			<Nav>
+			<ul className="Left">
+				<li className="OurStory" eventKey={1}>
+					<a href="OurStory.js">Our Story</a>
+				</li>
 
-				<NavItem className="OurStory" eventKey={1} href="">
-					Our Story
-				</NavItem>
+				<li className="Products" eventKey={2}>
+					<a href="Products.js">Products</a>
+				</li>
+			</ul>
+		
+			<ul className="Right">
+				<li className="LogIn" eventKey={3}>
+					<a href="Login.js">Log In</a>
+				</li>
 
-				<NavItem className="Products" eventKey={2} href="#">
-					Products
-				</NavItem>
-			</Nav>
+				<li className="SignUp" eventKey={4}>
+					<a href="Signup.js">Sign Up</a>
+				</li>
+			</ul>
 
-			<Nav pullRight>
-				<NavItem className="LogIn" eventKey={3} href="#">
-					Log In
-				</NavItem>
-
-				<NavItem className="SignUp" eventKey={4} href="#">
-					Sign Up
-				</NavItem>
-			</Nav>
-
-		</Navbar.Collapse>
-
-	</Navbar>
-	</div>
+		</div>
 
 
       	<ReactCSSTransitionGroup transitionName="fade" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
         </ReactCSSTransitionGroup>
 
-        	<img className="Home" src={home} alt="Welcome to Blank Slate" />
+        <div className="Home">	
 
         	<div className="Title">
         	<h1>Blank Slate</h1>
         	</div>
 
         	<div className="Tagline">
-        	<p>Your Mind, Unplugged</p>
+        	<p>Life, Unplugged</p>
         	</div>
    
+   		</div>
+
+
   				
 	 </div>
 
---
+++ src/OurStory.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './OurStory.css';
+
+class OurStory extends React.Component {
+	render() {
+		return(
+			<div className="OurStory">
+			<p>Awesome sauce</p>
+			</div>
+		)
+	}
+}
+
+export default OurStory;
